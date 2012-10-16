e3ab8b5f690dd2722e667c1cf2d4f878b3030778
Gunnar Selke <gunnar@gnnr.de>
2012-10-16 16:06:44 +0200

    added cyclic convolution: conv_simple and conv (fft)

---
+++ conv.py
@@ -0,0 +1,17 @@
+from fft import fft, ifft
+
+def conv_simple(a, b):
+  assert len(a) == len(b)
+  n = len(a)
+
+  c = [0] * n
+  for i in range(n):
+    c[i] = sum(a[j] * b[(n+i-j)%n] for j in range(n))
+  return c
+
+def conv(a, b):
+  assert len(a) == len(b)
+  n = len(a)
+
+  a, b = fft(a), fft(b)
+  return ifft(list(a[i] * b[i] / n for i in range(len(a))))
